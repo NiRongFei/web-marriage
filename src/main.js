0de7f453396dbd6c73296a8bd928441befeea42b
NiRongFei <fei332085762@aliyun.com>
2021-03-02 18:03:32 +0800

    feat: 开发页面顶部

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
-import "./styles/index.scss";
-import "element-ui/lib/theme-chalk/index.css";
+import './styles/index.scss';
+import 'element-ui/lib/theme-chalk/index.css';
 
-import Vue from "vue";
-import App from "./App.vue";
-import store from "./store";
-import router from "./router";
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
+import router from './router';
 
-import ElementUI from "element-ui";
+import ElementUI from 'element-ui';
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
-import * as filters from "./filters";
-Object.keys(filters).forEach(key => Vue.filter(key, filters[key]));
+import * as filters from './filters';
+Object.keys(filters).forEach((key) => Vue.filter(key, filters[key]));
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App),
+}).$mount('#app');
